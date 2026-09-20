f263507d33c0170b86051ba40e7bb6a652acd6da
Mohamed Cheikh Mohamed Cheikh <mohamed.cheikh@bumex.mr>
2026-09-20 13:18:11 +0000

    Fix admin authentication flow and rebuild assets

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,7 +15,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin/dashboard';
 
     public function boot()
     {
